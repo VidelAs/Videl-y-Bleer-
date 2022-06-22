05c6205852526992fb4b50d0e7d73a9573c8758e
Bleer Rodriguez Bolaños <65918783+Bleer30@users.noreply.github.com>
2022-06-22 10:03:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace kata2
+{
+    class Program
+    {
+        public static int DigitalRoot(long n)
+        {
+            //Este procedimiento hace un recuento de la cantidad de digitos que tiene un numero
+            int cant = 0;
+            long n2;
+            n2 = n;
+            do
+            {
+                n2 = n2 / 10;
+                cant = cant + 1;
+            } while (n2 > 0);
+            return cant;
+        }
+        static void Main(string[] args)
+        {
+            long n;
+            Console.WriteLine("digite el numero");
+            n = long.Parse(Console.ReadLine());
+            Console.WriteLine("La cantidad de digitos del numero es :" + DigitalRoot(n));
+        }
+    }
+}
